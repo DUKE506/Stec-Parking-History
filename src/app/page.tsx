8f35cc29fc5a704b8096b2e88a8fa39cf06dc554
duke <dukebs1128@gmail.com>
2025-03-13 16:59:42 +0900

    feat : next설치, shadcn적용

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,89 @@
-import Image from "next/image";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  columns,
+  ParkingHistory,
+} from "./_components/custom-data-table/columns";
+import CustomDataTable from "./_components/custom-data-table/custom-data-table";
 
-export default function Home() {
+async function getData(): Promise<ParkingHistory[]> {
+  // Fetch data from your API here.
+  return [
+    {
+      id: "1",
+      carType: "방문차량",
+      parkingType: "입차",
+      carNumber: "12가1111",
+      entryTime: "2023-03-10 09:01:12",
+      exitTime: "2023-03-10 18:03:18",
+      totalTime: "9시간 3분",
+      parkingAreaName: "일동미라주",
+      entryArea: "정문",
+      exitArea: "정문",
+      dong: "101",
+      ho: "202",
+      isBlack: false,
+      note: null,
+    },
+    {
+      id: "1",
+      carType: "방문차량",
+      parkingType: "입차",
+      carNumber: "12가2222",
+      entryTime: "2023-03-10 09:02:12",
+      exitTime: "2023-03-10 18:02:18",
+      totalTime: "9시간 1분",
+      parkingAreaName: "우방아파트",
+      entryArea: "정문",
+      exitArea: "정문",
+      dong: "101",
+      ho: "202",
+      isBlack: false,
+      note: null,
+    },
+    {
+      id: "1",
+      carType: "방문차량",
+      parkingType: "입차",
+      carNumber: "12가3333",
+      entryTime: "2023-03-10 09:03:12",
+      exitTime: "2023-03-10 18:01:18",
+      totalTime: "9시간 2분",
+      parkingAreaName: "에스텍",
+      entryArea: "정문",
+      exitArea: "정문",
+      dong: "101",
+      ho: "202",
+      isBlack: true,
+      note: null,
+    },
+  ];
+}
+
+export default async function Home() {
+  const data = await getData();
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+    <div>
+      <div className="container mx-auto py-10 ">
+        <Card className="p-8">
+          <CardHeader className="p-0">
+            <CardTitle className="text-lg">입출차 조회</CardTitle>
+          </CardHeader>
+          <CardContent className="p-0">
+            <CustomDataTable columns={columns} data={data} />
+          </CardContent>
+          <CardFooter>
+            <div className="flex-1 text-sm text-muted-foreground">
+              row(s) selected.
+            </div>
+          </CardFooter>
+        </Card>
+      </div>
     </div>
   );
 }
